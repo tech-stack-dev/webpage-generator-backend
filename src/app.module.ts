288eb145539584b6e0acffb2b1c4930e120951c8
Andrii Halai <andrii.halai@tech-stack.io>
2025-01-30 11:40:55 +0200

    Remove db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,20 +13,6 @@ import { OpenaiModule } from './openai/openai.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
-        synchronize: true,
-      }),
-    }),
     BasePagesModule,
     GeneratedPageModule,
     OpenaiModule,
