984ce5d959fffd4be8fc3fee5fe778de7b41b1a8
ValeriiRuchko <valerii.ruchko@tech-stack.io>
2025-05-05 15:48:38 +0200

    TSWEB-1985. Adapt start-up function for lambda-use. Update README. Add sls-offline support for local-lambda-simulated development. Add ability to optionally run as HTTP. Add API-prefix.

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,35 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import fastify, { FastifyInstance } from 'fastify';
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import awsLambdaFastify, {
+  LambdaResponse,
+  PromiseHandler,
+} from '@fastify/aws-lambda';
 
+const API_PREFIX = process.env.API_PREFIX;
+const START_AS_HTTP = process.env.START_AS_HTTP;
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
-async function bootstrap() {
+interface NestApp {
+  app: NestFastifyApplication;
+  instance: FastifyInstance;
+}
+
+let cachedNestApp: PromiseHandler<unknown, LambdaResponse>;
+
+// -----------------------------------------
+// NOTE: main bootstrap function where all of the configuration is going on
+async function bootstrap(): Promise<NestApp> {
+  // NOTE: initializing fastify instance with additional options
+
+  const instance: FastifyInstance = fastify();
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter(instance),
   );
 
   app.useGlobalPipes(
@@ -22,15 +43,60 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors();
-
-  await app.listen(PORT ?? 3000, HOST ?? '0.0.0.0');
-}
-
-bootstrap()
-  .then(() => {
-    console.log(`App listens at ${HOST}:${PORT}`);
-  })
-  .catch((err) => {
-    console.log('Error during application bootstrap', err);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    exposedHeaders: '*',
+    credentials: false,
+    methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
   });
+
+  // Set the prefix as necessary
+  app.setGlobalPrefix(API_PREFIX ?? 'api/v1');
+
+  await app.init();
+
+  return {
+    app,
+    instance,
+  };
+}
+// -----------------------------------------
+
+// -----------------------------------------
+// NOTE: if you want to NOT use 'sls offline' and instead spin-up backend as a regular service,
+// this section is responsible for that.
+// All is needed is setting START_AS_HTTP to 'true' value.
+const startAsHTTP = async function () {
+  const nestApp: NestApp = await bootstrap();
+  await nestApp.app.listen(PORT ?? 8080, HOST ?? '0.0.0.0');
+  console.log(`Listening at ${HOST} on port ${PORT}`);
+};
+
+if (START_AS_HTTP === 'true') {
+  startAsHTTP().catch((err) => {
+    console.log('Failed running locally:', err);
+  });
+}
+// -----------------------------------------
+
+// -----------------------------------------
+// NOTE: AWS-lambda handler that will be called
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+) => {
+  // If there's no cached app
+  if (!cachedNestApp) {
+    // Bootstrap
+    const nestApp: NestApp = await bootstrap();
+
+    // Create an AWS Lambda Fastify cached app from the Nest app
+    cachedNestApp = awsLambdaFastify(nestApp.instance, {
+      decorateRequest: true,
+    });
+  }
+
+  return cachedNestApp(event, context);
+};
+// -----------------------------------------
