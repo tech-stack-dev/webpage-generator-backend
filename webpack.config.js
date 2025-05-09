2e0b2850ebee8501fe62a011145b88ccb5a9601a
Ivan V <ivan.v@tech-stack.com>
2025-05-09 14:29:22 +0300

    Update Webpack config and deployment workflow for production readiness
    - Cleaned up commented-out code in webpack.config.js for better readability.
    - Changed GitHub Actions deployment branch from `iv_support-aws-deployment` to `main`.
    - Removed unused environment variables from the build job in deploy.yml.
    - Updated deployment variables to utilize GitHub secrets for improved security and flexibility.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.ts', // Adjust the entry file accordingly
+  entry: './src/main.ts',
   target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -11,17 +11,12 @@ module.exports = {
   externals: {},
   resolve: {
     extensions: ['.ts', '.js'],
-    // modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-    // alias: {
-    //   '@': path.resolve(__dirname, 'src'), // Create an alias for 'src'
-    // },
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        //exclude: /node_modules/,
       },
     ],
   },
